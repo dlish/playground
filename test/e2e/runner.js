861f0de5ce3b67108dd163b98d82506dc4855ac2
Dustin Lish <dlish27@gmail.com>
2018-02-13 23:02:53 -0700

    adding support for running e2e tests with docker

--- test/e2e/runner.js
+++ test/e2e/runner.js
@@ -28,7 +28,7 @@ devConfigPromise.then(devConfig => {
   let opts = process.argv.slice(2)
   if (opts.indexOf('--config') === -1) {
     opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js'])
-  }
+  } 
   if (opts.indexOf('--env') === -1) {
     opts = opts.concat(['--env', 'chrome'])
   }
